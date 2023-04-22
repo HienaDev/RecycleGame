d5586ee39a90b755f9e42c7de3b4ed154bac4518
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-04-22 17:43:23 +0100

    Drag and Drop no longer fails on fast movements

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,34 +4,65 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    //Initialize SerializeField variables
     [SerializeField]
     private Camera camera;
     [SerializeField]
     private LayerMask dragMask;
+
+    //Initialize variables
     private Vector3 mousePos;
     private Vector3 objectPos;
-    // Start is called before the first frame update
+    private bool dragging;
+    private Collider2D collider;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
+        //Convert mouse position to mouse position based on the camera
         mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButton(0))
+        //If the Left Mouse Button is pressed
+        if (Input.GetMouseButtonDown(0))
         {
+            //Create a new vector with the mouse position and the z axis at 0
             objectPos = new Vector3(mousePos[0], mousePos[1], 0f);
 
-            Collider2D collider = Physics2D.OverlapCircle(mousePos, 5, dragMask);
+            //Create a collider at the mouse position that checks for a draggable object
+            collider = Physics2D.OverlapCircle(mousePos, 5, dragMask);
             
+            //If it collided with a draggable object
             if (collider!=null)
             {
-                collider.transform.position = objectPos;
-                Debug.Log(objectPos);
+                //Set the dragging bool as true
+                dragging = true;
             }
+        }
+        //If the Left Mouse Button is released
+        else if(Input.GetMouseButtonUp(0))
+        {
+            //Set the dragging bool as false
+            dragging = false;
+        }
+
+        //While the Left Mouse Button is being held
+        if(dragging)
+        {
+            //Create a new vector with the mouse position and the z axis at 0
+            objectPos = new Vector3(mousePos[0], mousePos[1], 0f);
+            
+            //Move the draggable object to the mouse position (with the z axis at 0)
+            collider.transform.position = objectPos;
         }
     }
 }
